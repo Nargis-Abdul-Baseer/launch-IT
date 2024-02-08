efbfc7d9b309083624899138b03a5f8ac9a5703e
Nargis-Abdul-Baseer <130272364+Nargis-Abdul-Baseer@users.noreply.github.com>
2024-02-09 00:55:25 +0400

    Add files via upload

---
+++ SoundAscii/asciiLose.m
@@ -0,0 +1,16 @@
+
+function [] = asciiLose()
+    % ASCIILOSE function has an ascii art saying "YOU LOSE." The art was
+    % generated using https://patorjk.com/software/taag/#p=testall&h=3&v=0&f=Standard&t=here%20are%20your%20targets%20good%20luck!
+
+    disp('     )   )          (       )  (   ')     
+    disp('  ( /(( /(          )\ ) ( /(  )\ )     ')
+    disp('  )\())\())    (   (()/( )\())(()/((    ')
+    disp(' ((_)((_)\     )\   /(_)((_)\  /(_))\   ')
+    disp('__ ((_)((_) _ ((_) (_))   ((_)(_))((_)  ')
+    disp('\ \ / / _ \| | | | | |   / _ \/ __| __| ')
+    disp(' \ V | (_) | |_| | | |__| (_) \__ | _|  ')
+    disp('  |_| \___/ \___/  |____|\___/|___|___| ')
+                                            
+end
+
---
+++ SoundAscii/asciiWin.m
@@ -0,0 +1,16 @@
+
+function [] = asciiWin()
+    % ASCIIWIN function has an ascii art saying "YOU WIN." The art was
+    % generated using https://patorjk.com/software/taag/#p=testall&h=3&v=0&f=Standard&t=here%20are%20your%20targets%20good%20luck!
+
+    disp('      __      __  ______  __    __        __       __ ______ __    __  ')
+    disp('     |  \    /  \/      \|  \  |  \      |  \  _  |  |      |  \  |  \ ')
+    disp('      \$$\  /  $|  $$$$$$| $$  | $$      | $$ / \ | $$\$$$$$| $$\ | $$ ')
+    disp('       \$$\/  $$| $$  | $| $$  | $$      | $$/  $\| $$ | $$ | $$$\| $$ ')
+    disp('        \$$  $$ | $$  | $| $$  | $$      | $$  $$$\ $$ | $$ | $$$$\ $$ ')
+    disp('         \$$$$  | $$  | $| $$  | $$      | $$ $$\$$\$$ | $$ | $$\$$ $$ ')
+    disp('         | $$   | $$__/ $| $$__/ $$      | $$$$  \$$$$_| $$_| $$ \$$$$ ')
+    disp('         | $$    \$$    $$\$$    $$      | $$$    \$$|   $$ | $$  \$$$ ')
+    disp('          \$$     \$$$$$$  \$$$$$$        \$$      \$$\$$$$$$\$$   \$$ ')
+    
+end
